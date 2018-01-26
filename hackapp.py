672a9208728224f8d1e832e8287401778a629390
Danish Saeed <danishsaeed2@gmail.com>
2018-01-27 05:02:58 +0530

    Upload to server functionality

--- hackapp.py
+++ hackapp.py
@@ -9,15 +9,38 @@ import time, datetime
 import subprocess
 import pickle
 import math
-import csv
+from tkinter.filedialog import askopenfilename
+from shutil import copyfile
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 
+UPLOAD_FOLDER = os.path.dirname(os.path.abspath(__file__))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 dname = os.path.dirname(os.path.abspath(__file__))
 
+def open_file():
+	global file_name
+	file_name = askopenfilename(filetypes= (("ALL files","*.*"),("Image files","*jpg")))    
+	return file_name
+
+@app.route('/openfile',methods = ['POST','GET'])
+def openfile():
+	if request.method == 'POST':
+		return render_template("check.html",check=open_file())
+
+@app.route('/upload',methods=['POST','GET'])
+def upload():
+	if request.method == 'POST':
+		myfile = request.files['image']
+		f = os.path.join(app.config['UPLOAD_FOLDER']+'/temp/', myfile.filename)
+		myfile.save(f)
+		return render_template("hackapp.html")
+
 @app.route('/')
-def hello_world():
-    return render_template('hackapp.html')
+def home():
+	return render_template('hackapp.html')
 
 def dated_url_for(endpoint, **values):
     if endpoint == 'static':
